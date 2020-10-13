b09489a8571ce21d86f3a7ee6d57b8689f7b7d43
Steven West <Smwest87@gmail.com>
2020-10-13 11:33:29 -0500

    Updated to run locally

--- src/index.js
+++ src/index.js
@@ -1,17 +1,74 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Character extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state= {
+        Character: {
+          Id: null,
+        Name: null,
+        Class: null,
+        Level: null,
+        HitPointMaximum: null,
+        Strength: null,
+        Dexterity: null,
+        Constitution: null,
+        Wisdom: null,
+        Intelligence: null,
+        Charisma: null,
+        Initiative: null
+        }
+      }
+    }
+    componentDidMount() {
+      // Simple GET request using fetch
+      fetch('http://localhost:10000/character/41')
+          .then(response => response.json())
+          .then(data => this.setState({ Character: {
+            Id: data.Id,
+            Name: data.Name,
+            Class: data.Class,
+            Level: data.Level,
+            HitPointMaximum: data.HistPointMaximum,
+            Strength: data.Strength,
+            Dexterity: data.Dexterity,
+            Constitution: data.Constitution,
+            Wisdom: data.Wisdom,
+            Intelligence: data.Intelligence,
+            Charisma: data.Charisma,
+            Initiative: data.Initiative
+          } }));
+  }
+    render() {
+      return (
+        <div>
+        <h1>Character</h1>
+          <div>Id: {this.state.Character.Id}</div>
+          <div>Name: {this.state.Character.Name}</div>
+          <div>Class: {this.state.Character.Class}</div>
+          <div>Level: {this.state.Character.Level}</div>
+          <div>Hit Point Maximum: {this.state.Character.HitPointMaximum}</div>
+          <div>Strength: {this.state.Character.Strength}</div>
+          <div>Dexterity: {this.state.Character.Dexterity}</div>
+          <div>Constitution: {this.state.Character.Constitution}</div>
+          <div>Wisdom: {this.state.Character.Wisdom}</div>
+          <div>Intelligence: {this.state.Character.Intelligence}</div>
+          <div>Charisma: {this.state.Character.Charisma}</div>
+          <div>Initiative: {this.state.Character.Initiative}</div>
+          </div>
+      );
+    }
+  }
+  
+  
+  
+  
+  ReactDOM.render(
+    <Character/>,
+    document.getElementById('root')
+  );
+  
+  
+  
